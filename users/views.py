77ed8fd82b0012fdffc4ab15ac919412cd439239
adept <timofeysitnikov@mail.ru>
2025-05-30 14:49:27 +0300

    chopa

--- users/views.py
+++ users/views.py
@@ -3,12 +3,28 @@ from django.shortcuts import render, redirect
 from .models import *
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 
 # @login_required
 # def home(request):
 #     return render(request, 'users/unified_view.html')
 #     #return redirect('unified-feeds')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('unified-feeds')
+        else:
+            messages.error(request, "Неверное имя пользователя или пароль.")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
 def logout_view(request):
     logout(request)
     return redirect('login')
