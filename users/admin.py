31a750ed9a0adc4ba81bfc2436c855cd07f528b3
adept3101 <timofeysitnikov@mail.ru>
2025-06-14 19:10:57 +0300

    class

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, Permission
 from .models import (
     FeedsTypeNavigation,
     FeedsFinCertDateDownloads,
@@ -11,7 +12,7 @@ from .models import (
     FinCertIocDateProcessing
 )
 
-# Регистрация простых моделей
+
 admin.site.register(FeedsTypeNavigation)
 admin.site.register(FeedsFinCertDateDownloads)
 admin.site.register(FinCertIocDateProcessing)
@@ -30,3 +31,61 @@ admin.site.register(FeedsMvdCardNumbers)
 admin.site.register(FeedsMvdFastPayNumbers)
 admin.site.register(FeedsMvdInn)
 admin.site.register(FeedsMvdPassportHash)
+
+admin_group, _ = Group.objects.get_or_create(name='Admin')
+
+permission = Permission.objects.get(codename='add_user')
+
+admin_group.permissions.add(permission)
+
+analyst_group, _ = Group.objects.get_or_create(name='Analyst')
+
+network_admin_group, _ = Group.objects.get_or_create(name='Network Admin')
+
+user_group, _ = Group.objects.get_or_create(name='User')
+
+# try:
+#     permission = Permission.objects.get(codename='add_user')
+#     admin_group.permissions.add(permission)
+# except Permission.DoesNotExist:
+#     pass
+
+# # Админ-классы с ограничением доступа по ролям
+# def make_admin(model):
+#     @admin.register(model)
+#     class RoleBasedAdmin(admin.ModelAdmin):
+#         def has_view_permission(self, request, obj=None):
+#             return request.user.groups.filter(name__in=['Admin', 'Analyst']).exists()
+
+#         def has_module_permission(self, request):
+#             return self.has_view_permission(request)
+
+#         def has_change_permission(self, request, obj=None):
+#             return request.user.groups.filter(name='Admin').exists()
+
+#         def has_add_permission(self, request):
+#             return request.user.groups.filter(name='Admin').exists()
+
+#         def has_delete_permission(self, request, obj=None):
+#             return request.user.groups.filter(name='Admin').exists()
+#     return RoleBasedAdmin
+
+# # Применяем админ-классы ко всем моделям
+# make_admin(FeedsTypeNavigation)
+# make_admin(FeedsFinCertDateDownloads)
+# make_admin(FinCertIocDateProcessing)
+# make_admin(FeedsAccountNumbers)
+# make_admin(FeedsCardNumbers)
+# make_admin(FeedsEwalletNumbers)
+# make_admin(FeedsFastpayNumbers)
+# make_admin(FeedsInn)
+# make_admin(FeedsPassportHash)
+# make_admin(FeedsPhoneNumbers)
+# make_admin(FeedsSnilsHash)
+# make_admin(FeedsSwift)
+# make_admin(FeedsMvdDateDownloads)
+# make_admin(FeedsMvdAccountNumbers)
+# make_admin(FeedsMvdCardNumbers)
+# make_admin(FeedsMvdFastPayNumbers)
+# make_admin(FeedsMvdInn)
+# make_admin(FeedsMvdPassportHash)
--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,9 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.unified_feeds_view, name='unified-feeds'),
+    path('', views.page, name='unified-feeds'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', views.register_view, name='register'),
+    path('fincert/', views.FincertView.as_view(template_name='users/fincert.html'), name='fincert'),
 ]
